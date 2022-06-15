c57486e62b81a7bb2796c72e8899cc7e3d6882c5
Davide <69810644+ItzDavi@users.noreply.github.com>
2022-06-15 08:37:06 +0200

    Fixed Yes activity and layout

--- app/src/main/java/com/example/myapplication/Yes.kt
+++ app/src/main/java/com/example/myapplication/Yes.kt
@@ -2,10 +2,22 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Yes : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_yes)
+
+        val closeButton = findViewById<Button>(R.id.guudbai)
+        var clickCounter = 0
+
+        closeButton.setOnClickListener {
+            if (clickCounter >= 20) {
+                finish()
+            } else {
+                clickCounter++
+            }
+        }
     }
 }
